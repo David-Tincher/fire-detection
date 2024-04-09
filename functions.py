f30e653577185f4cc6f65b09dedefe05cd61e572
david <tincher.15@wright.edu>
2024-04-08 23:02:24 -0400

    Added statistic output

--- functions.py
+++ functions.py
@@ -17,6 +17,22 @@ def plot_confusion_matrix(cm, classes, normalize= False, title='Confusion matrix
         print('Confusion matix, without normalization')
 
     print(cm)
+
+    # Calculate accuracy
+    total_samples = np.sum(cm)
+    correct_predictions = np.sum(np.diag(cm))
+    accuracy = correct_predictions / total_samples
+    print("Detection Accuracy: {:.2f}%".format(accuracy * 100))
+
+    # Calculate precision and recall
+    precision = np.diag(cm) / np.sum(cm, axis=0)
+    recall = np.diag(cm) / np.sum(cm, axis=1)
+
+    for i, cls in enumerate(classes):
+        print("")
+        print("Precision for {}: {:.2f}%".format(cls, precision[i] * 100))
+        print("Recall for {}: {:.2f}%".format(cls, recall[i] * 100))
+
     thresh = cm.max() /2.
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
         plt.text(j,i, cm[i,j], horizontalalignment="center", color = "white" if cm[i, j] > thresh else "black")
